b56ff1b2557df3032587f91281190e9cc7dcb60b
Esteban Valente <valente.estebann@gmail.com>
2023-11-09 18:36:11 -0300

    feat: add new entities

---
+++ Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace url_shortener.Entities;
+
+public class User { 
+    [PrimaryKey, AutoIncrement]
+    public int Id {get; set;} 
+    public String Username {get; set;}
+    public String FirstName {get; set;}
+    public String LastName {get; set;}
+    public String Email {get; set;}
+        
+    [OneToMany(CascadeOperations = CascadeOperation.All)]
+    public List<XYZ> Urls { get; set; }
+}
---
+++ Entities/XYZ.cs
@@ -0,0 +1,15 @@
+﻿using SQLiteNetExtensions.Attributes;
+
+namespace url_shortener.Entities;
+
+public class XYZ
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string UrlShort { get; set; }
+    public string UrlLong { get; set; }
+    public int Clicks { get; set; }
+    public int CategoryId { get; set; }
+    [ForeignKey(typeof(User))]
+    public int UserId { get; set;}
+}
