d0f00254683fab2b83e05910df995817436f5c74
Esteban Valente <valente.estebann@gmail.com>
2023-11-09 18:35:33 -0300

    feat: create new data interfaces

---
+++ Data/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using url_shortener.Entities;
+
+namespace url_shortener.Models.Repository.Interface;
+
+public interface IUserService
+{
+    public List<User> GetUsers();
+    public User GetUser(int id);
+    public User GetUser(String email);
+    public void AddUser(UserForCreationDTO userForCreationDto);
+    public List<XYZ> GetUrls(int userId);
+    public void UpdateUser(UserForUpdateDTO userForUpdateDto);
+    public void DeleteUser(int userId);
+}
